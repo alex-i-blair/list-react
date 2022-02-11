72f4dc76d039e9b1b4ac9090645b29e937fbcc9e
Alex Blair <bclan11@gmail.com>
2022-02-10 17:25:05 -0800

    Cocktail component now working class

--- src/CocktailList/Cocktail.js
+++ src/CocktailList/Cocktail.js
@@ -1,8 +1,20 @@
-export default function Cocktail({ name, image }) {
-  return (
-    <div className="cocktail-items">
-      <h2>{name}</h2>
-      <img className="cocktail-image" src={image} />
-    </div>
-  );
+// export default function Cocktail({ name, image }) {
+//   return (
+//     <div className="cocktail-items">
+//       <h2>{name}</h2>
+//       <img className="cocktail-image" src={image} />
+//     </div>
+//   );
+// }
+import { Component } from 'react';
+
+export default class Cocktail extends Component {
+  render() {
+    return (
+      <div className="cocktail-items">
+        <h2>{this.props.name}</h2>
+        <img className="cocktail-image" src={this.props.image} />
+      </div>
+    );
+  }
 }
